b508f9114bd3495dd88f2054d7c75a401baa9de1
devlower <tuanne.assenco@gmail.com>
2023-01-23 16:03:42 -0300

    using localStorage from browser to sabe content / loading updated content on localhost

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  run: ['01-01', '01-02', '01-06', '01-13', '01-09', '01-07'],
-  takePills: ['01-03'],
-  journal: ['01-02']
+button.addEventListener('click', add) 
+form.addEventListener('change', save)
+
+function add() {
+
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if(!dayExists) {
+    nlwSetup.addDay(today)
+  } 
 }
 
+function save() {
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
